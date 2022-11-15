fc033d17e79aef6f12bee9a0df305a4c94283ce8
Ronivon Sampaio <ronivon.peixoto@gmail.com>
2022-11-15 17:53:37 -0300

    Adicionado testes unitários

--- src/test/java/com/apps/alticci/controller/AlticciControllerTest.java
+++ src/test/java/com/apps/alticci/controller/AlticciControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static java.lang.String.format;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -22,12 +23,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 class AlticciControllerTest {
 
+    private static final String ALTICCI_RESOURCE_URL = "/alticci/{index}";
+    private static final Long INVALID_INDEX_NUMBER = -1L;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     void getAlticciValueByIndex_WithInvalidIndexNumber_ShouldReturnBadRequest() throws Exception {
-        mockMvc.perform(get("/alticci/{index}", -1)
+        mockMvc.perform(get(ALTICCI_RESOURCE_URL, INVALID_INDEX_NUMBER)
                 .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
             .andExpect(status().isBadRequest())
@@ -36,13 +40,13 @@ class AlticciControllerTest {
             .andExpect(jsonPath("$.errors").isMap())
             .andExpect(jsonPath("$.errors", aMapWithSize(1)))
             .andExpect(jsonPath("$.errors", hasEntry("_getAlticciValueByIndex.index", "must be greater than or equal to 0")))
-            .andExpect(jsonPath("$.details", is("uri=/alticci/" + -1)));
+            .andExpect(jsonPath("$.details", is(format("uri=/alticci/%s", INVALID_INDEX_NUMBER))));
     }
 
     @ParameterizedTest
     @CsvSource({ "0, 0", "1, 1", "2, 1", "3, 1", "4, 2", "5, 2", "6, 3", "7, 4", "8, 5", "9, 7", "10, 9" })
     void getAlticciValueByIndex_WithValidIndexNumber_ShouldEqualTheExpectedReturn(int index, int valueExpected) throws Exception {
-        mockMvc.perform(get("/alticci/{index}", index)
+        mockMvc.perform(get(ALTICCI_RESOURCE_URL, index)
                 .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
             .andExpect(status().isOk())
