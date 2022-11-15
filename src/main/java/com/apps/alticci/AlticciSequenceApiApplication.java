7d5157e9463a026d2d95e21ad6e7424c14645c83
Ronivon Sampaio <ronivon.peixoto@gmail.com>
2022-11-15 10:24:47 -0300

    Estrutura inicial da aplicação

---
+++ src/main/java/com/apps/alticci/AlticciSequenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.apps.alticci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlticciSequenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlticciSequenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apps/alticci/AlticciSequenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.alticci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlticciSequenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
