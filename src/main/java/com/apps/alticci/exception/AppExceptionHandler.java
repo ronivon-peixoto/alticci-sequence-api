c319b7c0c496943081d49b70e82f2810267c849b
Ronivon Sampaio <ronivon.peixoto@gmail.com>
2022-11-15 17:44:39 -0300

    Adicionado testes unitários

--- src/main/java/com/apps/alticci/exception/AppExceptionHandler.java
+++ src/main/java/com/apps/alticci/exception/AppExceptionHandler.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.validation.ConstraintViolationException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class AppExceptionHandler {
@@ -20,6 +23,17 @@ public class AppExceptionHandler {
         return new ApiErrorsDto()
                 .timestamp(new Date())
                 .details(INTERNAL_SERVER_ERROR_MESSAGE);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ApiErrorsDto handleConstraintViolation(ConstraintViolationException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(e -> errors.put(e.getPropertyPath().toString(), e.getMessage()));
+        return new ApiErrorsDto()
+                .timestamp(new Date())
+                .errors(errors)
+                .details(request.getDescription(false));
     }
 
 }
--- src/test/java/com/apps/alticci/controller/AlticciControllerTest.java
+++ src/test/java/com/apps/alticci/controller/AlticciControllerTest.java
@@ -1,5 +1,6 @@
 package com.apps.alticci.controller;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,9 +25,23 @@ class AlticciControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
+    void getAlticciValueByIndex_WithInvalidIndexNumber_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(get("/alticci/{index}", -1)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.timestamp", is(notNullValue())))
+            .andExpect(jsonPath("$.errors").exists())
+            .andExpect(jsonPath("$.errors").isMap())
+            .andExpect(jsonPath("$.errors", aMapWithSize(1)))
+            .andExpect(jsonPath("$.errors", hasEntry("_getAlticciValueByIndex.index", "must be greater than or equal to 0")))
+            .andExpect(jsonPath("$.details", is("uri=/alticci/" + -1)));
+    }
+
     @ParameterizedTest
     @CsvSource({ "0, 0", "1, 1", "2, 1", "3, 1", "4, 2", "5, 2", "6, 3", "7, 4", "8, 5", "9, 7", "10, 9" })
-    void getAlticciSequenceValueByIndexShouldWorkGivenDifferentArguments(int index, int valueExpected) throws Exception {
+    void getAlticciValueByIndex_WithValidIndexNumber_ShouldEqualTheExpectedReturn(int index, int valueExpected) throws Exception {
         mockMvc.perform(get("/alticci/{index}", index)
                 .contentType(MediaType.APPLICATION_JSON))
             .andDo(print())
